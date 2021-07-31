5ec9e17005b3b79c2f881f90d20dbb9748fed442
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-31 20:10:51 +0200

    created new project

---
+++ Learn by Doing/Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Zdenko Čepan on 31/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
